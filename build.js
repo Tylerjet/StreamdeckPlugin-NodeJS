c262d78a0a1674ccfeaae4f70688f6c5560a1e50
MrTylerjet <MrTylerjet@gmail.com>
2020-05-12 18:28:15 -0700

    modified build script more, it now pulls the latest distributiontool from elgato, as well made 2 npm run commands one for building debug plugins and one for release plugins, that way you don't override the release plugin

--- build.js
+++ build.js
@@ -1,35 +1,62 @@
 const { exec } = require('pkg'),
 fs = require('fs'),
 { execFile } = require('child_process'),
+path = require('path'),
+http = require('https'),
+extract = require('extract-zip'),
 //Change Variables below to match your build info
-//Path to just before plugin folder
-DevPath = "D:\\DevelopmentProjects\\",
-//Name of the folder minuse .sdPlugin
-PluginName = "com.test",
-ExeName = "test.exe",
-PluginJS = "test.js"
-
+//Path to just before .sdPlugin folder that you are building
+devPath = "D:\\DevelopmentProjects\\", //make sure to add \\ at the end 
+//Name of the folder minus .sdPlugin
+pluginName = "com.test", //com.(name of plugin)
+exeName = "test.exe",
+pluginJS = "test.js", //name of script file if you are renaming it
+outputPath = process.argv[2];
 
 console.log("Building EXE");
 
-exec([PluginJS, '--target', 'win', '--output' ,ExeName]).then(()=> {
+exec([pluginJS, '--target', 'win', '--output' ,exeName]).then(()=> {
 	console.log("EXE Built!")
-	console.log("Copying File to "+DevPath+PluginName+".sdPlugin Folder");
-	fs.copyFile("test.exe",DevPath+PluginName+".sdPlugin\\"+ExeName, (err) =>{
+	console.log("Copying File to "+devPath+pluginName+".sdPlugin Folder");
+	fs.copyFile(exeName ,devPath+pluginName+".sdPlugin\\"+exeName, (err) =>{
 		if (err) {console.log(err)}
 		//console.log("File Copied!")
-		console.log("Deleting existing plugin")
-		fs.unlink(DevPath+'PluginOutput\\'+PluginName+'.streamDeckPlugin',(err) =>{
-			if (err) {
-				if(err.errno == -4058) {
-					console.log("Plugin already does not exist")
-				} else {console.log(error)}
+			if(fs.existsSync(devPath+outputPath) == false) {
+				fs.mkdir(devPath+outputPath, (err) => {
+					if(err) { 
+						if (err.code == "EEXIST") {
+							console.log("Folder Exists")
+						}
+						console.log(err)
+					}
+					console.log(devPath+outputPath+" folder created!")
+				})
 			}
-			console.log("Plugin Deleted")
-			console.log("Building New Plugin")
-			const child = execFile(DevPath+'DistributionTool.exe', ['-b','-i', DevPath+PluginName+'.sdPlugin','-o',DevPath+'PluginOutput'], (error, stdout, stderr) => {
-  				if (error) {console.log(error)}
-  				console.log(stdout)
+		fs.unlink(devPath+outputPath+'\\'+pluginName+'.streamDeckPlugin',(err) =>{
+			var exists = true;
+			if (err) {
+				if(err.code == "ENOENT") {
+					console.log("Plugin already does not exist")
+					exists = false
+				} else {console.log(err)}
+			}
+			if (exists) {console.log("Existing plugin deleted")}
+			const zipPath = path.resolve('./DistributionToolWindows.zip')
+			const file = fs.createWriteStream(zipPath);
+			console.log("Getting Distribution Tool")
+			http.get("https://developer.elgato.com/documentation/stream-deck/distributiontool/DistributionToolWindows.zip", (response) => {
+				response.pipe(file);
+				file.on("finish", () => {
+					console.log("Unzipping DistributionTool file");
+					extract('./DistributionToolWindows.zip', {dir: process.cwd()}).then(() => {
+						console.log("File Extracted")
+						console.log("Building New Plugin")
+						const child = execFile(/*devPath+*/'DistributionTool.exe', ['-b','-i', devPath+pluginName+'.sdPlugin','-o',devPath+outputPath], (err, stdout, stderr) => {
+		  				if (err) {console.log(err)}
+		  					console.log(stdout)
+						})
+					})
+				})
 			})
 		})
 	})
